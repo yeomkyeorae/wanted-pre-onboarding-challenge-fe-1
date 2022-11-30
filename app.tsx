943d8bd4841735b9018882c66608dcd89766a857
Kyeorae Yeom <duarufp06@gmail.com>
2022-11-30 23:59:45 +0900

    move app.tsx to src/

--- app.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-function App() {
-	return <div>It's React-TypeScript Template</div>;
-}
-
-export default App;
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+	return <div>It&apos;s React-TypeScript Template</div>;
+}
+
+export default App;
